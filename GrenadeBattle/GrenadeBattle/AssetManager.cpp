80fe7a204fef368852e8ed8a54250e0c89b2ed6e
cameron boyle <48211650+Ghillie-94@users.noreply.github.com>
2023-06-23 23:41:34 +0100

    Added asset manager class and declared functions and data

---
+++ GrenadeBattle/GrenadeBattle/AssetManager.cpp
@@ -0,0 +1,20 @@
+#include "AssetManager.h"
+
+sf::Texture& AssetManager::RequestTexture(std::string assetName)
+{
+	// TODO: insert return statement here
+}
+
+sf::Font& AssetManager::RequestFont(std::string assetName)
+{
+	// TODO: insert return statement here
+}
+
+sf::SoundBuffer& AssetManager::RequestSoundBuffer(std::string assetName)
+{
+	// TODO: insert return statement here
+}
+
+void AssetManager::DestroyAllAssets()
+{
+}
