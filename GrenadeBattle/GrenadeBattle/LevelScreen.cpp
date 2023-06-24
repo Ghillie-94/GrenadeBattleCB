afa902a8e7aba21973d29389754ad5035c4f70cb
cameron boyle <48211650+Ghillie-94@users.noreply.github.com>
2023-06-24 16:57:53 +0100

    Defined Restart and Reset play functions on level screen class

--- GrenadeBattle/GrenadeBattle/LevelScreen.cpp
+++ GrenadeBattle/GrenadeBattle/LevelScreen.cpp
@@ -38,8 +38,44 @@ void LevelScreen::AddGrenade()
 
 void LevelScreen::Restart()
 {
+	//clear the level in case it has already been running
+	for (int i = 0; i < grenades.size(); ++i)
+	{
+		delete grenades[i];
+		grenades[i] = nullptr;
+
+	}
+	grenades.clear();
+
+	for (int i = 0; i < platforms.size(); ++i)
+	{
+		delete platforms[i];
+		platforms[i] = nullptr;
+	}
+	platforms.clear();
+
+	//reset player lives
+	player1.SetLives(3);
+	player2.SetLives(3);
+
+	//reset positions
+	player1.SetPosition(30, 1000);
+	player2.SetPosition(800, 1000);
+
+
+	
 }
 
 void LevelScreen::ResetPlay()
 {
+	for (int i = 0; i < grenades.size(); ++i)
+	{
+		delete grenades[i];
+		grenades[i] = nullptr;
+
+	}
+	grenades.clear();
+
+	player1.SetPosition(30, 1000);
+	player2.SetPosition(800, 1000);
 }
