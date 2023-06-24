f9e6a6fbc4d47edf03504545a1cb0154c586583a
cameron boyle <48211650+Ghillie-94@users.noreply.github.com>
2023-06-24 01:32:34 +0100

    Added End panel class and declared its data and functions

---
+++ GrenadeBattle/GrenadeBattle/EndPanel.cpp
@@ -0,0 +1,29 @@
+#include "EndPanel.h"
+
+EndPanel::EndPanel(sf::RenderWindow& newWindow)
+{
+}
+
+void EndPanel::Update(sf::Time frameTime)
+{
+}
+
+void EndPanel::Draw(sf::RenderTarget& target)
+{
+}
+
+void EndPanel::SetPosition(sf::Vector2f newPos)
+{
+}
+
+void EndPanel::StartAnimation()
+{
+}
+
+void EndPanel::ResetPosition()
+{
+}
+
+void EndPanel::SetWinner(std::string newWinner)
+{
+}
---
+++ GrenadeBattle/GrenadeBattle/EndPanel.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+
+class EndPanel
+{
+public:
+	EndPanel(sf::RenderWindow& newWindow);
+
+	void Update(sf::Time frameTime);
+	void Draw(sf::RenderTarget& target);
+
+	void SetPosition(sf::Vector2f newPos);
+
+	void StartAnimation();
+	void ResetPosition();
+
+	void SetWinner(std::string newWinner);
+
+protected:
+	std::string winner;
+
+private:
+
+	sf::Sprite background;
+	sf::Text title;
+	sf::Text message;
+	sf::Vector2f position;
+	sf::RenderWindow* window;
+	bool animatingIn;
+	sf::Clock animationClock;
+	
+};
+
