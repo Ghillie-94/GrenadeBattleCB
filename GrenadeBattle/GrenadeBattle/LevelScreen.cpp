177e90ab7929c31ef888a25d0ab06c5edcae2444
cameron boyle <48211650+Ghillie-94@users.noreply.github.com>
2023-06-24 14:16:27 +0100

    TODO: create screen class constructor

--- GrenadeBattle/GrenadeBattle/LevelScreen.cpp
+++ GrenadeBattle/GrenadeBattle/LevelScreen.cpp
@@ -1,1 +1,29 @@
 #include "LevelScreen.h"
+
+LevelScreen::LevelScreen(Game* newGamePointer)
+{
+}
+
+void LevelScreen::Update(sf::Time frameTime)
+{
+}
+
+void LevelScreen::Draw(sf::RenderTarget& target)
+{
+}
+
+void LevelScreen::TriggerEndState(bool win, std::string _winner)
+{
+}
+
+void LevelScreen::AddGrenade()
+{
+}
+
+void LevelScreen::Restart()
+{
+}
+
+void LevelScreen::ResetPlay()
+{
+}
