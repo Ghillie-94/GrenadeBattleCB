0e4033c08fce6d5b00d26d3eb33b89e794a72ec5
cameron boyle <48211650+Ghillie-94@users.noreply.github.com>
2023-06-24 01:42:36 +0100

    Added easing namespace and declared its functions

---
+++ GrenadeBattle/GrenadeBattle/Easing.cpp
@@ -0,0 +1,11 @@
+#include "Easing.h"
+
+sf::Vector2f Easing::Linear(sf::Vector2f begin, sf::Vector2f change, float duration, float time)
+{
+	return sf::Vector2f();
+}
+
+sf::Vector2f Easing::EaseInQuad(sf::Vector2f begin, sf::Vector2f change, float duration, float time)
+{
+	return sf::Vector2f();
+}
