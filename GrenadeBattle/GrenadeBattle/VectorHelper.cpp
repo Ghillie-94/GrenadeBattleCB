7434a087fd058dd82b1f8692d2b5b12767f7670e
cameron boyle <48211650+Ghillie-94@users.noreply.github.com>
2023-06-24 14:33:27 +0100

    Added vector helper namespace and declared its functions

---
+++ GrenadeBattle/GrenadeBattle/VectorHelper.cpp
@@ -0,0 +1,16 @@
+#include "VectorHelper.h"
+
+float VectorHelper::SquareMagnitude(sf::Vector2f vec)
+{
+    return 0.0f;
+}
+
+float VectorHelper::Magnitude(sf::Vector2f vec)
+{
+    return 0.0f;
+}
+
+sf::Vector2f VectorHelper::Normalise(sf::Vector2f vec)
+{
+    return sf::Vector2f();
+}
