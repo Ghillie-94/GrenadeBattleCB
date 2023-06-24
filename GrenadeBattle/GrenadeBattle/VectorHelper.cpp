ab02c1c0a238e90f3baf65e2d99c39c29bda768d
cameron boyle <48211650+Ghillie-94@users.noreply.github.com>
2023-06-24 14:36:16 +0100

    defined vector helper's functions

--- GrenadeBattle/GrenadeBattle/VectorHelper.cpp
+++ GrenadeBattle/GrenadeBattle/VectorHelper.cpp
@@ -2,15 +2,21 @@
 
 float VectorHelper::SquareMagnitude(sf::Vector2f vec)
 {
-    return 0.0f;
+    return vec.x * vec.x + vec.y * vec.y;
 }
 
 float VectorHelper::Magnitude(sf::Vector2f vec)
 {
-    return 0.0f;
+    return sqrt(SquareMagnitude(vec));
 }
 
 sf::Vector2f VectorHelper::Normalise(sf::Vector2f vec)
 {
-    return sf::Vector2f();
+	float mag = Magnitude(vec);
+
+	vec.x = vec.x / mag;
+	vec.y = vec.y / mag;
+
+
+	return vec;
 }
