a9bd14111e59e48d2181d330fb06ef2f121a5058
cameron boyle <48211650+Ghillie-94@users.noreply.github.com>
2023-06-26 22:23:43 +0100

    vector mafs

--- GrenadeBattle/GrenadeBattle/VectorHelper.cpp
+++ GrenadeBattle/GrenadeBattle/VectorHelper.cpp
@@ -20,3 +20,32 @@ sf::Vector2f VectorHelper::Normalise(sf::Vector2f vec)
 
 	return vec;
 }
+
+float VectorHelper::DotProduct(float vec1X, float vec1Y, float vec2X, float vec2Y)
+{
+	return (vec1X * vec2X) + (vec1Y * vec2Y);
+}
+
+sf::Vector3f VectorHelper::CrossProduct3D(float vec1X, float vec1Y, float vec1Z, float vec2X, float vec2Y, float vec2Z)
+{
+	sf::Vector3f crossProduct;
+	crossProduct.x = vec1Y * vec2Z - vec1Z * vec2Y;
+	crossProduct.y = vec1Z * vec2X - vec1X * vec2Z;
+	crossProduct.z = vec1X * vec2Y - vec1Y * vec2X;
+	return crossProduct;
+
+}
+
+sf::Vector2f VectorHelper::Reflect(sf::Vector2f incoming, sf::Vector2f normal)
+{
+	sf::Vector2f reflection;
+	
+	reflection = incoming - (2.0f * normal) * (DotProduct(incoming.x, incoming.y, normal.x , normal.y));
+	return reflection;
+}
+
+
+
+
+
+
--- GrenadeBattle/GrenadeBattle/VectorHelper.h
+++ GrenadeBattle/GrenadeBattle/VectorHelper.h
@@ -7,5 +7,11 @@ namespace VectorHelper
 	float Magnitude(sf::Vector2f vec);
 
 	sf::Vector2f Normalise(sf::Vector2f vec);
+	float DotProduct(float vec1X, float vec1Y, float vec2X, float vec2Y);
+	sf::Vector3f CrossProduct3D(float vec1X, float vec1Y, float vec1Z, float vec2X, float vec2Y, float vec2Z);
+
+	sf::Vector2f Reflect(sf::Vector2f incoming, sf::Vector2f normal);
+
+
 };
 
